a8966671ec4586815b76b2a45e41add64105a829
laluuquang16102000 <laluuquang16102000@gmail.com>
2020-11-09 15:08:11 +0700

    update api point

--- src/main/java/com/enao/team2/quanlynhanvien/service/AccountService.java
+++
@@ -1,9 +0,0 @@
-package com.enao.team2.quanlynhanvien.service;
-
-import com.enao.team2.quanlynhanvien.model.Account;
-import com.enao.team2.quanlynhanvien.security.UserPrincipal;
-
-public interface AccountService {
-    UserPrincipal findByUsername(String username);
-    Account createUser(Account account);
-}
